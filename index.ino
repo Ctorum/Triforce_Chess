4b86a66fc4d13a32a2cb527386844076b80e3404
Ctorum <vhcabralrezende@hotmail.com>
2021-03-30 15:40:57 -0400

    Recognize Individual Reed Switches

--- index.ino
+++ index.ino
@@ -6,6 +6,8 @@ String squares[4] = {"A1", "A2", "B1", "B2"};
 #define C2 3
 #define C3 4
 #define C4 5
+#define C5 6
+#define C6 7
 
 void setup()
 {
@@ -14,14 +16,18 @@ void setup()
   pinMode(C2, INPUT);
   pinMode(C3, INPUT);
   pinMode(C4, INPUT);
+  pinMode(C5, INPUT);
+  pinMode(C6, INPUT);
 }
 
 void loop()
 {
-  Serial.println("CASA 1: " + String(digitalRead(C1)));
-  Serial.println("CASA 2: " + String(digitalRead(C2)));
-  Serial.println("CASA 3: " + String(digitalRead(C3)));
-  Serial.println("CASA 4: " + String(digitalRead(C4)));
+  Serial.println("X1 - (1): " + String(digitalRead(C1)));
+  Serial.println("X2 - (2): " + String(digitalRead(C2)));
+  Serial.println("X3 - (3): " + String(digitalRead(C3)));
+  Serial.println("Y1 - (4): " + String(digitalRead(C4)));
+  Serial.println("Y2 - (5): " + String(digitalRead(C5)));
+  Serial.println("Y3 - (6): " + String(digitalRead(C6)));
   Serial.println("----------------------------------");
   delay(1000);
 }
