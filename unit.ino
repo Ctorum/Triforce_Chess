c5ab9a97d91f8bca07d716ae581c8e4e04e9cfe3
Ctorum <vhcabralrezende@hotmail.com>
2021-04-01 18:32:18 -0400

    New ports

--- unit.ino
+++ unit.ino
@@ -6,18 +6,21 @@
 #define C4 5
 #define C5 6
 #define C6 7
+#define C7 8
+#define C8 9
 
 int xPos = 0;
 int yPos = 0;
 
 void getCooridanates()
 {
-    for (int i = 2; i < 5; i++)
+    for (int i = 2; i < 6; i++)
     {
         int i_verification = digitalRead(i);
+        /*Comment here to house analysis
         if (i_verification == 1)
         {
-            for (int j = 5; j < 8; j++)
+            for (int j = 6; j < 10; j++)
             {
                 int j_verification = digitalRead(j);
                 if (j_verification == 1)
@@ -27,8 +30,11 @@ void getCooridanates()
             }
             xPos = i;
         }
+        Finish coment here*/
+        Serial.println(i + ": " + i_verification);
     }
-    Serial.println(String(xPos) + ";" + String(yPos));
+    //Serial.println(String(xPos) + ";" + String(yPos));
+    Serial.println("-----------------------------");
 }
 
 void setup()
@@ -40,6 +46,8 @@ void setup()
     pinMode(C4, INPUT);
     pinMode(C5, INPUT);
     pinMode(C6, INPUT);
+    pinMode(C7, INPUT);
+    pinMode(C8, INPUT);
 }
 
 void loop()
