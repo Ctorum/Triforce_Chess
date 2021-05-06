d3aa428ecd5d6a38d0b7cdd43d7ae5ed5f9924b6
Ctorum <vhcabralrezende@hotmail.com>
2021-05-06 13:49:42 -0400

    L Read

--- unit.ino
+++ unit.ino
@@ -1,79 +1,75 @@
-#include <string.h>
+#define boardWidth 4
 
-#define ARRAY_MAX 40
-#define C1 2
-#define C2 3
-#define C3 4
-#define C4 5
-#define C5 6
-#define C6 7
-#define C7 8
-#define C8 9
+int powerPins[4] = {50,51,52,53};
 
-int xPos = 0;
-int yPos = 0;
+String positions[boardWidth * boardWidth];
 
-int xPosAux = 0;
-int yPosAux = 0;
+String letterPositions[8] = {"A","B","C","D","E","F","G","H"};
+String numberPositions[8] = {"1","2","3","4","5","6","7","8"};
 
-String coordinates[8] = {"1", "2", "3", "4", "A", "B", "C", "D"};
-String movesX[] = {};
-String movesY[] = {};
-unsigned int plays = 1;
-unsigned int movesX_length = 0;
-unsigned int movesY_length = 0;
+void setup() {
+  Serial.begin(115200);
+  for (int x : powerPins) {
+    pinMode(x, OUTPUT);
+    digitalWrite(x, LOW);
+  }
 
-void getCoordinates()
-{
-    do
-    {
-        for (int i = 0; i < 64; i++)
-        {
-            xPosAux = xPos;
-            yPosAux = yPos;
-            for (int i = 2; i < 6; i++)
-            {
-                int i_verification = digitalRead(i);
-                //Comment here to house analysis
-                if (i_verification == 1)
-                {
-                    for (int j = 6; j < 10; j++)
-                    {
-                        int j_verification = digitalRead(j);
-                        if (j_verification == 1)
-                        {
-                            xPos = j;
-                        }
-                    }
-                    yPos = i;
-                }
-                //Finish coment here
-                //Serial.println(i + ": " + i_verification)
-            }
+  for (int i = 30; i < 38; i++) {
+    pinMode(i, INPUT);
+  }
+}
 
-            if (xPos != xPosAux || yPos != yPosAux)
-            {
-                Serial.println(movesX[movesX_length] + movesY[movesY_length]);
-            }
+void readHouses() {
+  for (int i = 0; i < sizeof(positions)/sizeof(positions[0]); i++)
+  {
+    positions[i] = "";
+  }
+  
+  int horizontalInitializer = 34;
+  int verticalInitializer = 30;
+  
+  for (int x : powerPins) {
+    digitalWrite(x, HIGH);
+
+    for(int y = 0; y < boardWidth; y++) {
+      if (digitalRead(verticalInitializer + y) == HIGH) {
+        int horizontalState[boardWidth];
+        
+        for(int j = 0; j < boardWidth; j++) {
+          int state = digitalRead(horizontalInitializer + j);
+          if (state == HIGH) {
+            horizontalState[j] = 1;
+          } else {
+            horizontalState[j] = 0;
+          }
         }
-        plays++;
-    } while (plays <= ARRAY_MAX);
-}
-void setup()
-{
-    Serial.begin(115200);
-    pinMode(C1, INPUT);
-    pinMode(C2, INPUT);
-    pinMode(C3, INPUT);
-    pinMode(C4, INPUT);
-    pinMode(C5, INPUT);
-    pinMode(C6, INPUT);
-    pinMode(C7, INPUT);
-    pinMode(C8, INPUT);
+
+        for (int e = 0; e < boardWidth; e++) {
+          if (horizontalState[e] == 1) {
+            for (int p = 0; p < sizeof(positions)/sizeof(positions[0]); p++) {
+              if (positions[p] == NULL) {
+                positions[p] = String(letterPositions[y] + numberPositions[y]);
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    digitalWrite(x, LOW);
+  }
+
+  for(String pos : positions) {
+    if (pos != NULL) {
+      Serial.println(pos);
+    }
+  }
+
+//  Serial.println(positions[0]);
 }
 
-void loop()
-{
-    getCoordinates();
-    delay(1000);
+void loop() {
+  readHouses();
+  delay(2500);
 }
